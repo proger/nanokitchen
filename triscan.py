277285a6529f5eeefa90140f35cc6bfe8c713c8f
Volodymyr Kyrylov <vol@wilab.org.ua>
2023-12-29 01:05:22 +0200

    benchmark tl.associative_scan

--- triscan.py
+++ triscan.py
@@ -10,13 +10,16 @@ def scan1(
     outputs,
     SEQUENCE_LENGTH: tl.constexpr,
 ):
+    global_id = tl.num_programs(axis=1) * tl.program_id(axis=0) + tl.program_id(axis=1)
+    offset = global_id * SEQUENCE_LENGTH
+
     out = 0.
     for i in range(SEQUENCE_LENGTH):
         if i == 0:
-            out = tl.load(tokens + i)
+            out = tl.load(tokens + offset + i)
         else:
-            out = tl.load(gates + i) * out + tl.load(tokens + i)
-        tl.store(outputs + i, out)
+            out = tl.load(gates + offset + i) * out + tl.load(tokens + offset + i)
+        tl.store(outputs + offset + i, out)
 
 @triton.jit
 def scan2(
@@ -39,13 +42,58 @@ def scan2(
                 out = tl.load(gates + offset + i) * out + tl.load(tokens + offset + i)
             tl.store(outputs + offset + i, out)
 
+# from https://github.com/openai/triton/issues/2359
+@triton.jit
+def unpack64(merged):
+    tl.static_assert(merged.dtype == tl.int64)
+    b = (merged & 0xFFFFFFFF).to(tl.int32).to(tl.float32, bitcast=True)
+    a = (merged >> 32).to(tl.int32).to(tl.float32, bitcast=True)
+    return a, b
+
+@triton.jit
+def pack64(a, b):
+    tl.static_assert(a.dtype == tl.float32)
+    tl.static_assert(b.dtype == tl.float32)
+    a = a.to(dtype=tl.int32, bitcast=True).to(tl.int64)
+    a = a << 32  # shifted by 32 bits
+    b = b.to(dtype=tl.int32, bitcast=True).to(tl.int64)
+    return a | b
+
+@triton.jit
+def scan_op(l, r):
+    xl, fl = unpack64(l)
+    xr, fr = unpack64(r)
+    x = xl * fr + xr
+    f = fl * fr
+    return pack64(x, f)
+
+@triton.jit
+def scan3(
+    gates,
+    tokens,
+    outputs,
+    SEQUENCE_LENGTH: tl.constexpr,
+):
+    global_id = tl.num_programs(axis=1) * tl.program_id(axis=0) + tl.program_id(axis=1)
+    offsets = tl.arange(0, SEQUENCE_LENGTH) + global_id * SEQUENCE_LENGTH
+
+    tokens_ = tl.load(tokens + offsets)
+    gates_ = tl.load(gates + offsets)
+
+    tuples = pack64(tokens_, gates_)
+    output_tuples = tl.associative_scan(tuples, 0, combine_fn=scan_op)
+    output_tokens, output_gates = unpack64(output_tuples)
+    tl.store(outputs + offsets, output_tokens)
+
 @triton.testing.perf_report([
     triton.testing.Benchmark(
         x_names=["SEQUENCE_LENGTH"],  # argument names to use as an x-axis for the plot
         x_vals=[2**i for i in range(17)],
         line_arg="provider",  # argument name whose value corresponds to a different line in the plot
-        line_vals=["scan1", "scan2", "eager"],  # argument values to use as different lines in the plot
-        line_names=["scan1", "scan2", "eager"],  # legend entries to use for each line
+        #line_vals=["scan1", "scan2", "tl.associative_scan", "eager"],  # argument values to use as different lines in the plot
+        #line_names=["scan1", "scan2", "tl.associative_scan", "eager"],  # legend entries to use for each line
+        line_vals=["scan1", "scan2", "tl.associative_scan"],
+        line_names=["scan1", "scan2", "tl.associative_scan"],
         plot_name="scan1",  # name of the plot
         args={
             #"SEQUENCE_LENGTH": seq_len,
@@ -74,7 +122,7 @@ def scan2(
     #     }
     # ),
 ])
-def bench(provider, SEQUENCE_LENGTH, UNROLL_LENGTH=16, device="cuda"):
+def bench(provider, SEQUENCE_LENGTH, UNROLL_LENGTH=64, device="cuda"):
     B, C, T = 1, 1, SEQUENCE_LENGTH
     gates, tokens = init(B, C, T, device)
     outputs = torch.empty_like(tokens)
@@ -84,6 +132,8 @@ def bench(provider, SEQUENCE_LENGTH, UNROLL_LENGTH=16, device="cuda"):
             scan = lambda: scan1[(1,)](gates, tokens, outputs, SEQUENCE_LENGTH)
         case "scan2":
             scan = lambda: scan2[(1,)](gates, tokens, outputs, SEQUENCE_LENGTH, UNROLL_LENGTH)
+        case "tl.associative_scan":
+            scan = lambda: scan3[(1,)](gates, tokens, outputs, SEQUENCE_LENGTH)
         case "eager":
             scan = lambda: scan_eager(gates, tokens, outputs)
         case "compile":
@@ -96,12 +146,10 @@ def bench(provider, SEQUENCE_LENGTH, UNROLL_LENGTH=16, device="cuda"):
     ms = triton.testing.do_bench(scan, warmup=25, rep=100)
     return ms
 
-
 def init(B, C, T, device):
     gates = 0.999 + 0.001 * torch.rand(B, C, T, device=device)
     tokens = torch.rand(B, C, T, device=device)
     return gates, tokens
-
 
 def scan_eager(gates, tokens, outputs):
     _, _, T = gates.shape
@@ -110,7 +158,6 @@ def scan_eager(gates, tokens, outputs):
             outputs[:, :, i] = tokens[:, :, i]
         else:
             outputs[:, :, i] = gates[:, :, i] * outputs[:, :, i-1] + tokens[:, :, i]
-
 
 def test_allclose():
     device = 'cuda'
@@ -141,6 +188,20 @@ def test_allclose2():
     scan_eager(gates, tokens, outputs_naive)
 
     assert torch.allclose(outputs, outputs_naive)
+
+def test_allclose3():
+    device = 'cuda'
+    B, C, T = 1,1, 512
+    gates, tokens = init(B, C, T, device)
+
+    outputs = torch.empty_like(tokens)
+    scan2[(B,C)](gates, tokens, outputs, T, UNROLL_LENGTH=16)
+
+    outputs3 = torch.empty_like(tokens)
+    scan3[(B,C)](gates, tokens, outputs3, T)
+    print(outputs)
+    print(outputs3)
+    assert torch.allclose(outputs, outputs3, atol=1e-1)
 
 if __name__ == '__main__':
     bench.run(save_path=".", print_data=True)
