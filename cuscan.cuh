9e378ff3805cc5c8952c93fedcfb03756325877e
Volodymyr Kyrylov <vol@wilab.org.ua>
2024-01-02 18:22:55 +0200

    make cuscan parametric

--- cuscan.cuh
+++ cuscan.cuh
@@ -19,40 +19,42 @@
 #include <c10/cuda/CUDAException.h>  // For C10_CUDA_CHECK and C10_CUDA_KERNEL_LAUNCH_CHECK
 #include <torch/extension.h>
 
-using input_t = float;
-using weight_t = float;
-using scan_t = float2;
-using vec_t = uint4; // For loading 4 items at a time
-static_assert(sizeof(vec_t) == 16);
+template<int kNThreads_, int kNItems_>
+struct Kernel_traits {
+    using input_t = float;
+    using weight_t = float;
+    using scan_t = float2;
+    using vec_t = uint4; // For loading 4 items at a time
+    static_assert(sizeof(vec_t) == 16);
 
-static constexpr int kNThreads = 32;
-static constexpr int kNItems = 4;
-static_assert(kNItems % 4 == 0);
-    // if (params.seqlen <= 128) {
-    //     selective_scan_fwd_launch<32, 4, input_t, weight_t>(params, stream);
-    // } else if (params.seqlen <= 256) {
-    //     selective_scan_fwd_launch<32, 8, input_t, weight_t>(params, stream);
-    // } else if (params.seqlen <= 512) {
-    //     selective_scan_fwd_launch<32, 16, input_t, weight_t>(params, stream);
-    // } else if (params.seqlen <= 1024) {
-    //     selective_scan_fwd_launch<64, 16, input_t, weight_t>(params, stream);
-    // } else {
-    //     selective_scan_fwd_launch<128, 16, input_t, weight_t>(params, stream);
-    // }
-static constexpr int kNBytes = sizeof(input_t);
-static_assert(kNBytes == 4);
-static constexpr int kNElts = kNBytes == 4 ? 4 : std::min(8, kNItems);
-static constexpr int kNLoads = kNItems / kNElts;
-static_assert(kNItems % kNElts == 0);
-static_assert(kNLoads == 1);
-static_assert(kNItems == 4);
-// Setting MinBlocksPerMP to be 3 (instead of 2) for 128 threads improves occupancy.
-static constexpr int kMinBlocks = 3;
+    static constexpr int kNThreads = kNThreads_;
+    static constexpr int kNItems = kNItems_;
+    static_assert(kNItems % 4 == 0);
+    static constexpr int kNBytes = sizeof(input_t);
+    static_assert(kNBytes == 4);
+    static constexpr int kNElts = kNBytes == 4 ? 4 : std::min(8, kNItems);
+    static constexpr int kNLoads = kNItems / kNElts;
+    static_assert(kNItems % kNElts == 0);
+    static_assert(kNLoads == 1);
+    // Setting MinBlocksPerMP to be 3 (instead of 2) for 128 threads improves occupancy.
+    static constexpr int kMinBlocks = kNThreads < 128 ? 5 : 3;
 
-template<typename scalar_t> struct SSMScanOp;
+    using BlockLoadT = cub::BlockLoad<input_t, kNThreads, kNItems, cub::BLOCK_LOAD_WARP_TRANSPOSE>;
+    using BlockLoadVecT = cub::BlockLoad<vec_t, kNThreads, kNLoads, cub::BLOCK_LOAD_DIRECT>;
+    using BlockStoreT = cub::BlockStore<input_t, kNThreads, kNItems, cub::BLOCK_STORE_WARP_TRANSPOSE>;
+    using BlockStoreVecT = cub::BlockStore<vec_t, kNThreads, kNLoads, cub::BLOCK_STORE_DIRECT>;
+    using BlockScanT = cub::BlockScan<scan_t, kNThreads, cub::BLOCK_SCAN_WARP_SCANS>;
+    static constexpr int kSmemIOSize = std::max({sizeof(typename BlockLoadT::TempStorage),
+                                                    sizeof(typename BlockLoadVecT::TempStorage),
+                                                    sizeof(typename BlockStoreT::TempStorage),
+                                                    sizeof(typename BlockStoreVecT::TempStorage)});
+    static constexpr int kSmemSize = kSmemIOSize + sizeof(typename BlockScanT::TempStorage);
+};
+
+template<typename scalar_t> struct FirstOrderScanOp;
 
 template<>
-struct SSMScanOp<float> {
+struct FirstOrderScanOp<float> {
     __device__ __forceinline__ float2 operator()(const float2 &ab0, const float2 &ab1) const {
         return make_float2(ab1.x * ab0.x, ab1.x * ab0.y + ab1.y);
     }
@@ -60,30 +62,19 @@ struct SSMScanOp<float> {
 
 // A stateful callback functor that maintains a running prefix to be applied
 // during consecutive scan operations.
-template <typename scalar_t> struct SSMScanPrefixCallbackOp {
+template <typename scalar_t> struct FirstOrderScanPrefixCallbackOp {
     using scan_t = std::conditional_t<std::is_same_v<scalar_t, float>, float2, float4>;
     scan_t running_prefix;
     // Constructor
-    __device__ SSMScanPrefixCallbackOp(scan_t running_prefix_) : running_prefix(running_prefix_) {}
+    __device__ FirstOrderScanPrefixCallbackOp(scan_t running_prefix_) : running_prefix(running_prefix_) {}
     // Callback operator to be entered by the first warp of threads in the block.
     // Thread-0 is responsible for returning a value for seeding the block-wide scan.
     __device__ scan_t operator()(scan_t block_aggregate) {
         scan_t old_prefix = running_prefix;
-        running_prefix = SSMScanOp<scalar_t>()(running_prefix, block_aggregate);
+        running_prefix = FirstOrderScanOp<scalar_t>()(running_prefix, block_aggregate);
         return old_prefix;
     }
 };
-
-using BlockLoadT = cub::BlockLoad<input_t, kNThreads, kNItems, cub::BLOCK_LOAD_WARP_TRANSPOSE>;
-using BlockLoadVecT = cub::BlockLoad<vec_t, kNThreads, kNLoads, cub::BLOCK_LOAD_DIRECT>;
-using BlockStoreT = cub::BlockStore<input_t, kNThreads, kNItems, cub::BLOCK_STORE_WARP_TRANSPOSE>;
-using BlockStoreVecT = cub::BlockStore<vec_t, kNThreads, kNLoads, cub::BLOCK_STORE_DIRECT>;
-using BlockScanT = cub::BlockScan<scan_t, kNThreads, cub::BLOCK_SCAN_WARP_SCANS>;
-static constexpr int kSmemIOSize = std::max({sizeof(typename BlockLoadT::TempStorage),
-                                                sizeof(typename BlockLoadVecT::TempStorage),
-                                                sizeof(typename BlockStoreT::TempStorage),
-                                                sizeof(typename BlockStoreVecT::TempStorage)});
-static constexpr int kSmemSize = kSmemIOSize + sizeof(typename BlockScanT::TempStorage);
 
 struct Params {
     int batch;
@@ -102,14 +93,23 @@ struct Params {
     void* out_ptr;
 };
 
-__global__ __launch_bounds__(kNThreads, kMinBlocks)
+template <typename Ktraits>
+__global__ __launch_bounds__(Ktraits::kNThreads, Ktraits::kMinBlocks)
 void simple_scan_kernel(Params params) {
+    using input_t = typename Ktraits::input_t;
+    using weight_t = typename Ktraits::weight_t;
+    using scan_t = typename Ktraits::scan_t;
+    using vec_t = typename Ktraits::vec_t;
+    constexpr int kNThreads = Ktraits::kNThreads;
+    constexpr int kNItems = Ktraits::kNItems;
+    constexpr int kNLoads = Ktraits::kNLoads;
+
     // Shared memory.
     extern __shared__ char smem_[];
-    auto& smem_load_vec = reinterpret_cast<BlockLoadVecT::TempStorage&>(smem_);
-    auto& smem_store_vec = reinterpret_cast<BlockStoreVecT::TempStorage&>(smem_);
-    auto& smem_scan = *reinterpret_cast<BlockScanT::TempStorage*>(smem_ + kSmemIOSize);
-    scan_t *smem_running_prefix = reinterpret_cast<scan_t *>(smem_ + kSmemSize);
+    auto& smem_load_vec = reinterpret_cast<typename Ktraits::BlockLoadVecT::TempStorage&>(smem_);
+    auto& smem_store_vec = reinterpret_cast<typename Ktraits::BlockStoreVecT::TempStorage&>(smem_);
+    auto& smem_scan = *reinterpret_cast<typename Ktraits::BlockScanT::TempStorage*>(smem_ + Ktraits::kSmemIOSize);
+    scan_t *smem_running_prefix = reinterpret_cast<scan_t *>(smem_ + Ktraits::kSmemSize);
 
     const int batch_id = blockIdx.x;
     const int dim_id = blockIdx.y;
@@ -120,7 +120,6 @@ void simple_scan_kernel(Params params) {
     scan_t *x = reinterpret_cast<scan_t *>(params.x_ptr) + (batch_id * params.dim + dim_id) * params.n_chunks;
 
     constexpr int kChunkSize = kNThreads * kNItems;
-    constexpr int state_idx = 0;
     for (int chunk = 0; chunk < params.n_chunks; ++chunk) {
         input_t tokens_vals[kNItems], gates_vals[kNItems];
         __syncthreads();
@@ -147,18 +146,18 @@ void simple_scan_kernel(Params params) {
 
         // Initialize running total
         // If we use WARP_SCAN then all lane 0 of all warps (not just thread 0) needs to read
-        scan_t running_prefix = chunk > 0 && threadIdx.x % 32 == 0 ? smem_running_prefix[state_idx] : make_float2(1.f, 0.f);
+        scan_t running_prefix = chunk > 0 && threadIdx.x % 32 == 0 ? *smem_running_prefix : make_float2(1.f, 0.f);
 
-        SSMScanPrefixCallbackOp<weight_t> prefix_op(running_prefix);
+        FirstOrderScanPrefixCallbackOp<weight_t> prefix_op(running_prefix);
         cub::BlockScan<scan_t, kNThreads, cub::BLOCK_SCAN_WARP_SCANS>(smem_scan).InclusiveScan(
-            thread_data, thread_data, SSMScanOp<weight_t>(), prefix_op
+            thread_data, thread_data, FirstOrderScanOp<weight_t>(), prefix_op
         );
 
         // There's a syncthreads in the scan op, so we don't need to sync here.
         // Unless there's only 1 warp, but then it's the same thread (0) reading and writing.
         if (threadIdx.x == 0) {
-            smem_running_prefix[state_idx] = prefix_op.running_prefix;
-            x[chunk + state_idx] = prefix_op.running_prefix;
+            *smem_running_prefix = prefix_op.running_prefix;
+            x[chunk] = prefix_op.running_prefix;
         }
 
         __syncthreads();
@@ -181,16 +180,22 @@ void simple_scan_kernel(Params params) {
     }
 }
 
+template <int kNThreads, int kNItems>
 void simple_scan_cuda(Params &params, cudaStream_t stream) {
     assert(params.seqlen % (kNThreads * kNItems) == 0);
-    constexpr int kSmemSizeTotal = kSmemSize + sizeof(scan_t);
-    // printf("smem_size = %d\n", kSmemSizeTotal);
+    using Ktraits = Kernel_traits<kNThreads, kNItems>;
+    auto kernel = &simple_scan_kernel<Ktraits>;
+
+    constexpr int kSmemSize = Ktraits::kSmemSize + sizeof(typename Ktraits::scan_t);
+    // printf("smem_size = %d\n", kSmemSize);
     dim3 grid(params.batch, params.dim);
-    if (kSmemSizeTotal >= 48 * 1024) {
+
+    if (kSmemSize >= 48 * 1024) {
         C10_CUDA_CHECK(cudaFuncSetAttribute(
-            simple_scan_kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, kSmemSizeTotal));
+            kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, kSmemSize));
     }
-    simple_scan_kernel<<<grid, kNThreads, kSmemSizeTotal, stream>>>(params);
+
+    kernel<<<grid, kNThreads, kSmemSize, stream>>>(params);
     C10_CUDA_KERNEL_LAUNCH_CHECK();
 }
 
@@ -214,8 +219,8 @@ simple_scan_forward(const at::Tensor &tokens, const at::Tensor &gates) {
     CHECK_SHAPE(tokens, batch_size, dim, seqlen);
     CHECK_SHAPE(gates, batch_size, dim, seqlen);
 
-    const int n_chunks = (seqlen + 512 - 1) / 512;
-    // const int n_chunks = (seqlen + 2048 - 1) / 2048;
+    // const int n_chunks = (seqlen + 512 - 1) / 512;
+    const int n_chunks = (seqlen + 2048 - 1) / 2048;
     // const int n_chunks = (seqlen + 1024 - 1) / 1024;
     // at::Tensor out = torch::empty_like(tokens);
     // Right now tokens have BHL layout and gates has HBL layout, and we want out to have HBL layout
@@ -244,7 +249,19 @@ simple_scan_forward(const at::Tensor &tokens, const at::Tensor &gates) {
     // Cast to char to avoid compiler warning about narrowing
     at::cuda::CUDAGuard device_guard{(char)tokens.get_device()};
     auto stream = at::cuda::getCurrentCUDAStream().stream();
-    simple_scan_cuda(params, stream);
+
+    // XXX: kNItems is hardcoded to 4, as we would like to always use BLOCK_STORE_DIRECT for now
+    if (params.seqlen <= 128) {
+        simple_scan_cuda<32, 4>(params, stream);
+    } else if (params.seqlen <= 256) {
+        simple_scan_cuda<64, 4>(params, stream);
+    } else if (params.seqlen <= 512) {
+        simple_scan_cuda<128, 4>(params, stream);
+    } else if (params.seqlen <= 1024) {
+        simple_scan_cuda<256, 4>(params, stream);
+    } else {
+        simple_scan_cuda<512, 4>(params, stream);
+    }
     std::vector<at::Tensor> result = {out, x};
     return result;
 }
